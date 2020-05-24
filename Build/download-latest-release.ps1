25d3b3f4f664c455aaed380afca95a3d515836b4
Evgeny Gunko <evgeny.gunko@gmail.com>
2020-05-24 17:36:28 +0000

    Merged PR 109: Create yam file for branch policy build
    Related work items: #50

--- Build/download-latest-release.ps1
+++ Build/download-latest-release.ps1
@@ -1,16 +1,23 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$githubpersonaltoken
+)
+
 # Download latest release artifacts from github
 $repo = "evgenygunko/CopyWords.Parsers"
 
 $releases = "https://api.github.com/repos/$repo/releases"
+$token = $githubpersonaltoken | ConvertTo-SecureString -AsPlainText -Force
 
 Write-Host Determining latest release
-$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+$tag = (Invoke-WebRequest -Uri $releases -Authentication OAuth -Token $token | ConvertFrom-Json)[0].tag_name
 $file = "CopyWords.Parsers.Evgeny.$tag.nupkg"
 
 $downloadUrl = "https://github.com/$repo/releases/download/$tag/$file"
 Write-Host "downloadUrl = $downloadUrl"
 
-$targetFolder = "$env:BUILD_SOURCESDIRECTORY/.nuget/local_nuget"
+$targetFolder = "$($env:AGENT_BUILDDIRECTORY)/Nuget/local_nuget"
 #$targetFolder = "/Temp/packages"
 $targetFile = "$targetFolder/$file"
 Write-Host "targetFile = $targetFile"
